354b0c3d6dc6687af8ab1a385e88f547bc7499fe
Amirkz01 <112914666+Amirkz01@users.noreply.github.com>
2022-09-07 13:38:15 +0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -31,18 +31,18 @@ module.exports = {
         global.img = 'https://telegra.ph/file/0c567eac403737766688f.jpg'
         global.fla = pickRandom(global.flaaa)
         global.namabot = conn.user.name
-        global.packname = '© DaniBoTz-MD'
-        global.author = 'Made By Dani Official © 2022'
+        global.packname = '© XyzenBoTz'
+        global.author = 'Made By Amirr © 2022'
         global.packname2 = 'Made With'
-        global.author2 = 'DaniBoTz-MD By Dani Official © 2022'
+        global.author2 = 'DaniBoTz-MD By Amirr © 2022'
         global.wm3 = '·͟͟͟͟͟͟͞͞͞͞͞͞Made By Dani Official˚'
-        global.wm2 = '© DaniBoTz-MD' + ' ' + 'By Dani Official'
-        global.wm = '© DaniBoTz-MD'
+        global.wm2 = '© XyzenBOTz' + ' ' + 'By Amirr'
+        global.wm = '© XyzenBoTz'
         global.colong1 = 'Ciss 📸'
         global.colong2 = 'By Haori & Ziv San'
         global.kontak2 = [
-        ['6281251549899', 'Dani Official', 'Creator Bot', 'Dani@support.com', true],
-        ['6285727529001', 'DaniBoTz-MD', 'Bot WhatsApp', 'Just Bot', true]
+        ['6282154156241', 'Amirr', 'Creator Bot', 'Amir@support.com', true],
+        ['6285727529001', 'XyzenBOTz', 'Bot WhatsApp', 'Just Bot', true]
         ]
         global.bg = await (await fetch(img)).buffer()
         global.time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
